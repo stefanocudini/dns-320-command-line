fa13cfb247c87e8ccb338a6af5bc5efe670f3505
z4k <none@none>
2012-10-11 20:59:39 +0200

    fixed help parameter

--- pulse.php
+++ pulse.php
@@ -12,37 +12,33 @@ requirements: php5-cli,	php5-curl
 
 define('DEBUG', false);
 
-define('HELP',"
 
-Usage: pulse.php options [host[:port]]
-
-       host                        hostname or ip target, default: pulse
-       port                        port number for host, default: 80
-OPTIONS:
-       -p,--p2p[=on|off]           get or set p2p client state
-       -c,--p2p-clear              clear p2p complete list
-       -l,--p2p-limit[=down[,up]]  get or set p2p speed limit, unlimit: -1
-       -s,--p2p-start[=id,id,...]  start all or specific torrent download
-       -o,--p2p-stop[=id,id,...]   stop all or specific torrent download
-       -x,--p2p-delete[=id,id,...] delete specific torrent download
-       -D,--download[=url]         list or add url in http downloader
-       -C,--download-clear         clear complete http downloads list
-       -L,--download-list[=file]   add urls from file list
-       -n,--nfs[=on|off]           get or set nfs service
-       -f,--ftp[=on|off]           get or set ftp service
-       -t,--temp                   get temperature inside
-       -T,--time                   get date and time of nas
-       -F,--fan[=off|low|high]     get or set fan mode
-       -u,--ups                    get ups state
-       -U,--usb                    get usb disk/flash info
-       -M,--usb-umount             umount usb disk/flash
-       -d,--disks                  get disks usage
-       -S,--shutdown               power off system now
-       -P,--shutdown-prog          get list schedule power off
-       -r,--restart                restart system
-       -h,--help                   print this help
-
-");
+#$nameOptions = array(
+#'p2p' => array('p', '::'),
+#'p2p-clear'  => array('c', ''),
+#'p2p-limit'  => array('l', '::'),
+#'p2p-start'  => array('s', '::'),
+#'p2p-stop'   => array('o', '::'),
+#'p2p-delete' =>  array('x', '::'),
+#'download::' =>  array('D', '::'),
+#'download-clear' =>  array('C', ''),
+#'download-list:' =>  array('L', ':'),
+#'nfs::' =>  array('N', '::'),
+#'ftp::' =>  array('F', '::'),
+#'temp'  =>  array('t', ''),
+#'time'  =>  array('T', ''),
+#'fan::' =>  array('a', '::'),
+#'ups'   =>  array('u', ''),
+#'usb'   =>  array('U', ''),
+#'usb-umount' =>  array('M', ''),
+#'disks'      =>  array('d', ''),
+#'shutdown'   =>  array('S', ''),
+#'shutdown-prog' =>  array('P', ''),
+#'restart' =>  array('r', ''),
+#'force'   =>  array('f', ''),
+#'help'    =>  array('h', '')
+#);
+		
 $options = array(
 		'p::'=> 'p2p::',
 		'c'  => 'p2p-clear',
@@ -67,6 +63,38 @@ $options = array(
 		'r'  => 'restart',
 		'f'  => 'force',
 		'h'  => 'help');
+
+define('HELP',"
+
+Usage: pulse.php options [host[:port]]
+
+       host                        hostname or ip target, default: pulse
+       port                        port number for host, default: 80
+OPTIONS:
+       -p,--p2p[=on|off]           get or set p2p client state
+       -c,--p2p-clear              clear p2p complete list
+       -l,--p2p-limit[=down[,up]]  get or set p2p speed limit, unlimit: -1
+       -s,--p2p-start[=id,id,...]  start all or specific torrent download
+       -o,--p2p-stop[=id,id,...]   stop all or specific torrent download
+       -x,--p2p-delete[=id,id,...] delete specific torrent download
+       -D,--download[=url]         list or add url in http downloader
+       -C,--download-clear         clear complete http downloads list
+       -L,--download-list[=file]   add urls from file list
+       -N,--nfs[=on|off]           get or set nfs service
+       -F,--ftp[=on|off]           get or set ftp service
+       -t,--temp                   get temperature inside
+       -T,--time                   get date and time of nas
+       -a,--fan[=off|low|high]     get or set fan mode
+       -u,--ups                    get ups state
+       -U,--usb                    get usb disk/flash info
+       -M,--usb-umount             umount usb disk/flash
+       -d,--disks                  get disks usage
+       -S,--shutdown               power off system now
+       -P,--shutdown-prog          get list schedule power off
+       -r,--restart                restart system
+       -h,--help                   print this help
+
+");
 
 if(version_compare(PHP_VERSION, '5.3.0', '<'))
 	$opts = getopt(implode('',array_keys($options)));
